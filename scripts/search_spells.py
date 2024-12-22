1fa96e85cf373f4d6e974b1b8cb516201ccae1ac
Diogo Regateiro <diogo.regateiro@gmail.com>
2024-12-22 18:50:39 +0000

    Update Summoner spell list.

--- scripts/search_spells.py
+++ scripts/search_spells.py
@@ -1,4 +1,5 @@
 import utils
+import sys
 
 def sort_by_name(e):
     return e['name']
@@ -11,13 +12,16 @@ spells = utils.load_data("homebrew/ishiir-spells.json")
 spells["spell"].sort(key = sort_by_name)
 spells["spell"].sort(key = sort_by_level)
 
-level = 0
-print(level)
+level = -1
+count = 0
 for spell in spells["spell"]:
     for _class in spell.get("classes", {}).get("fromClassList", []):
-        if _class["name"] == "Summoner":
+        if _class["name"].lower() == sys.argv[1].lower():
             if spell["level"] != level:
                 level = spell["level"]
-                print()
-                print(level)
+                print("\n" + str(level))
+
+            count = count + 1
             print(spell["name"])
+
+print(f"\nTotal: {count}")
