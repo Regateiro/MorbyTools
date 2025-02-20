6b892b31b5a113c0181c97073ec1a1a8f96fce14
Diogo Regateiro <diogo.regateiro@atlar.pt>
2025-02-20 11:11:07 +0000

    Sort things out in the homebrew files.

--- scripts/sort.py
+++ scripts/sort.py
@@ -6,6 +6,13 @@ SORT_KEY = ""
 
 def sort_by(e):
     global SORT_KEY
+
+    if SORT_KEY == "crafted":
+        return min(e.get("crafted", {}).get("skill", [""]))
+
+    if SORT_KEY == "value":
+        return e.get("value", 0)
+
     return e[SORT_KEY]
 
 items = utils.load_data(sys.argv[1])
