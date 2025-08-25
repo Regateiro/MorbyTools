72b2e3433bb6e402e4d2d0942de30346f42c5088
Diogo Regateiro <diogo.regateiro@atlar.pt>
2025-08-25 16:12:02 +0100

    Try to filter alphabetically.

--- js/filter-spells.js
+++ js/filter-spells.js
@@ -350,7 +350,7 @@ class PageFilterSpells extends PageFilter {
 		this._optionalfeaturesFilter = new SearchableFilter({header: "Other Option/Feature"});
 		this._metaFilter = new Filter({
 			header: "Components & Miscellaneous",
-			items: sort([...PageFilterSpells._META_FILTER_BASE_ITEMS, "Ritual", "SRD", "Basic Rules", "Legacy", "Has Images", "Has Token"]),
+			items: [...PageFilterSpells._META_FILTER_BASE_ITEMS, "Ritual", "SRD", "Basic Rules", "Legacy", "Has Images", "Has Token"].sort(),
 			isMiscFilter: true,
 			displayFn: it => Parser.spMiscTagToFull(it),
 		});
